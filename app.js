7676fd4519265751277386b6f0a8496ccfcee31a
Andrew Horn <arhorn@smcm.edu>
2021-12-24 03:54:01 -0600

    added helmetjs, altered route

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const helmet = require('helmet')
 
 require('./scraper') // activate scraper
 
-var indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index')
 
-var app = express()
+const app = express()
+
+app.use(helmet())
 
 app.use(logger('dev'))
 app.use(express.json())
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const scraper = require('../scraper')
 
-router.get('/', async (req, res, next) => {
+router.get('/api/v1/renewal-locations', async (req, res, next) => {
   const renewalLocations = await scraper()
   res.send(renewalLocations)
 })
