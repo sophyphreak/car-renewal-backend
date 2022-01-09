5a231c1d8506f831eb7e01efca92ab9e1da00741
Andrew Horn <arhorn@smcm.edu>
2022-01-09 03:31:07 -0600

    updatePlaces now correctly deletes table before repopulating it

--- prisma/updatePlaces.js
+++ prisma/updatePlaces.js
@@ -3,13 +3,20 @@ const { PrismaClient } = pkg
 
 const prisma = new PrismaClient()
 
-const updatePlaces = renewalLocations => {
-  prisma.$executeRaw('DROP TABLE IF EXISTS Place')
-  renewalLocations.forEach(async place => {
-    await prisma.place.create({
-      data: place,
+const updatePlaces = async renewalLocations => {
+  try {
+    // TODO:
+    // make it so this only deletes missing entries and adds new entries
+    // -> will require changing other files as well
+    await prisma.place.deleteMany({})
+    renewalLocations.forEach(async place => {
+      await prisma.place.create({
+        data: place,
+      })
     })
-  })
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 export default updatePlaces
